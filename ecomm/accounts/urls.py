832df01ce6a9f0ba78c27d7c86642fecbc08cfbc
Raheel-Izaz <raheelizaz3@gmail.com>
2025-08-19 11:10:44 +0500

    register added and login pending

---
+++ ecomm/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+app_name = "accounts"
+urlpatterns = [
+    path('login/', views.loginView,name= "login"),
+    path('register/', views.registerView,name= "register")
+]
--- ecomm/accounts/views.py
+++ ecomm/accounts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def loginView(request):
+    return render(request, "login.html")
+
+
+def registerView(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user_obj = User.objects.filter(username=email)
+        if user_obj.exists():
+            messages.warning(request, "Emial already exists!")
+            return HttpResponseRedirect(request.path_info)
+        user_obj = User.objects.create(
+            first_name=first_name, last_name=last_name, email=email, username=email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        messages.success(request,"An email has been sent to you mail for verification")
+        return HttpResponseRedirect(request.path_info)
+    return render(request, "register.html")
